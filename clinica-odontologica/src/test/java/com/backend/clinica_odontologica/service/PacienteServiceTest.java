7371a6d2b3856a2e4c198285574a78b82643444a
Jesrig P. Alxedo <Jesrig.pineda@alxedo.mx>
2024-06-20 02:29:35 -0600

    feat - pruebas unitarias restantes

--- clinica-odontologica/src/test/java/com/backend/clinica_odontologica/service/PacienteServiceTest.java
+++ clinica-odontologica/src/test/java/com/backend/clinica_odontologica/service/PacienteServiceTest.java
@@ -1,4 +1,55 @@
-package com.backend.clinica_odontologica.service;
+package com.backend.clinica_odontologica.service.impl;
 
+import com.backend.clinica_odontologica.dto.entrada.DomicilioEntradaDto;
+import com.backend.clinica_odontologica.dto.entrada.PacienteDtoEntrada;
+import com.backend.clinica_odontologica.dto.salida.PacienteDtoSalida;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class PacienteServiceTest {
+
+    @Autowired
+    private PacienteService pacienteService;
+
+    @Test
+    @Order(1)
+    void deberiaRegistrarseUnPacienteDeNombreJuan_yRetornarSuId(){
+
+        PacienteDtoEntrada pacienteDtoEntrada = new PacienteDtoEntrada(6845485, "Julio", "Pino", LocalDate.of(2024, 6, 20), new DomicilioEntradaDto("Lomas Mar", 100, "Lomas", "Coyuca"));
+
+        PacienteDtoSalida pacienteDtoSalida = pacienteService.guardarPaciente(pacienteDtoEntrada);
+
+        //assert
+        assertNotNull(pacienteDtoSalida);
+        assertNotNull(pacienteDtoSalida.getId());
+        assertEquals("Julio", pacienteDtoSalida.getNombre());
+    }
+
+    @Test
+    @Order(2)
+    void deberiaDevolverUnaListaNoVaciaDePacientes(){
+        List<PacienteDtoSalida> listadoDePacientes = pacienteService.listarTodosLosPacientes();
+        assertFalse(listadoDePacientes.isEmpty());
+    }
+
+    @Test
+    @Order(3)
+    void deberiaEliminarseElPacienteConId1(){
+
+        assertDoesNotThrow(() -> pacienteService.eliminarPaciente(1L));
+    }
 }
--- clinica-odontologica/src/test/java/com/backend/clinica_odontologica/service/TurnosServiceTest.java
+++ clinica-odontologica/src/test/java/com/backend/clinica_odontologica/service/TurnosServiceTest.java
@@ -1,4 +1,55 @@
-package com.backend.clinica_odontologica.service;
+package com.backend.clinica_odontologica.service.impl;
 
+import com.backend.clinica_odontologica.dto.entrada.DomicilioEntradaDto;
+import com.backend.clinica_odontologica.dto.entrada.OdontologoDtoEntrada;
+import com.backend.clinica_odontologica.dto.entrada.PacienteDtoEntrada;
+import com.backend.clinica_odontologica.dto.entrada.TurnoDtoEntrada;
+import com.backend.clinica_odontologica.dto.salida.TurnoDtoSalida;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TurnosServiceTest {
+    @Autowired
+    private PacienteService pacienteService;
+
+    @Autowired
+    private OdontologoService odontologoService;
+
+    @Autowired
+    private TurnosService turnosService;
+
+    @Test
+    @Order(1)
+    void deberiaDevolverUnaListaNoVaciaDeTurnos(){
+        List<TurnoDtoSalida> listadoDeTurnos = turnosService.listarTodosLosTurnos();
+        assertFalse(listadoDeTurnos.isEmpty());
+    }
+
+    @Test
+    @Order(2)
+    void deberiaEliminarseElTurnoConId1(){
+
+        assertDoesNotThrow(() -> turnosService.eliminarTurno(1L));
+    }
+
+    @Test
+    @Order(3)
+    void deberiaDevolverUnaListaVaciaDeTurnos(){
+        List<TurnoDtoSalida> listadoDeTurnos = turnosService.listarTodosLosTurnos();
+        assertTrue(listadoDeTurnos.isEmpty());
+    }
 }
